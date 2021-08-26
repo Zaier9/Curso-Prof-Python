d8992a00c1cec0730cf5df358ac75558c324a8c7
Zaier Vera <veraalber989@gmail.com>
2021-08-26 17:41:53 -0300

    make_division

---
+++ make_division.py
@@ -0,0 +1,16 @@
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+def make_divisor_by(n: int) -> Generic[T]:
+    def numerator(x: int) -> float:
+        assert n != 0, "You can't divide by zero."
+        return x/n
+    return numerator
+
+def main():
+    divided_by_2 = make_divisor_by(2)
+    print(divided_by_2(10))
+
+if __name__ == '__main__':
+    main()
