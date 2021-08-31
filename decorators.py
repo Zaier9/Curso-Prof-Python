375f707ad0736b2d343b9ae336fd6c3a128224e4
Zaier Vera <veraalber989@gmail.com>
2021-08-31 15:52:39 -0300

    Seguimos

--- decorators.py
+++ decorators.py
@@ -1,10 +1,28 @@
-def mayusculas(func):
-    def envoltura(texto):
-        return func(texto).upper()
-    return envoltura
+from datetime import datetime
 
-@mayusculas
-def mensaje(nombre):
-    return f'{nombre}, recibiste un mensaje'
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        initial_time = datetime.now()
+        func(*args, **kwargs)
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print("Pasaron" + str(time_elapsed.total_seconds()) + " segundos")
+    return wrapper
 
-print(mensaje('Cesar'))
+@execution_time
+def random_func():
+    for _ in range(1, 100000000):
+        pass
+
+@execution_time
+def suma(a: int, b: int) -> int:
+    return a + b
+
+@execution_time
+def saludo(nombre="Cesar"):
+    print("Hola " + nombre)
+
+random_func()
+suma(5, 5)
+saludo("Facundo")
+        
