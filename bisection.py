6a4aa9d030630131d0560a0e168e1d7e5d5c1ddc
Zaier Vera <veraalber989@gmail.com>
2021-08-31 17:17:25 -0300

    Mas practica

---
+++ bisection.py
@@ -0,0 +1,35 @@
+from typing import Callable
+
+def bisection(function: Callable[[float], float], a: float, b: float) -> float:
+    start: float = a
+    end: float = b
+    if function(a) == 0: #one of the a or b is a root for the function
+        return a
+    elif function(b) == 0:
+        return b
+    elif (function(a) * function(b) > 0):
+    #if none of these are root and they are both positive or negative,
+    #then this algorithm can't find the root
+        raise ValueError('could not find root in given interval.')
+    else:
+        mid: float = start + (end - start) / 2.0
+        while abs(start - mid) > 10 ** 7: 
+            if function(mid) == 0:
+                return mid
+            elif function(mid) * function(start) < 0:
+                end = mid
+            else:
+                start = mid
+            mid = start + (end - start) / 2.0
+        return mid
+
+
+def f(x: float) -> float:
+    return x ** 3 - 2 * x - 5
+
+if __name__ == '__main__':
+    print(bisection(f, 1, 10000))
+
+    
+
+            
