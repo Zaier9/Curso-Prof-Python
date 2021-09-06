7d64bfd0ef15eb385b398e51a362ae72e668d180
Zaier Vera <veraalber989@gmail.com>
2021-09-06 16:26:05 -0300

    Agregando lineas de codigo al juego

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -79,3 +79,196 @@ class Block(pygame.sprite.Sprite):
 
     def redraw(self):
         self._draw(self.x, self.y)
+
+    def _create_mask(self):
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def initial_daw(self):
+        raise NotImplementedError
+
+    @property
+    def group(self):
+        return self.groups() [0]
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+        self.rect.left = value*TILE_SIZE
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
+        self.rect.top = value*TILE_SIZE
+
+    def move_left(self, group):
+        self.x -= 1
+        if self.x < 0 or Block.collide(self, group):
+            self.x += 1
+
+    def move_right(self, group):
+        self.x += 1
+        if self.rect.right < GRID_WIDTH or Block.collide(self, group):
+            self.x -= 1
+
+    def move_down(self, group):
+        self.y += 1
+        if self.rect.bottom > GRID_HEIGHT or Block.collide(self, group):
+            self.y -= 1
+            self.current = False
+            raise BottomReached
+
+    def rotate(self, group):
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.rect.width = self.image.get_width()
+        self.rect.height = self.image.get_height()
+        self._create_mask()
+
+        while self.rect.right > GRID_WIDTH:
+            self.x -= 1
+
+        while self.rect.left < 0:
+            self.x += 1
+
+        while self.rect.bottom > GRID_HEIGHT:
+            self.y -= 1
+
+        while True:
+            if not Block.collide(self, group):
+                break
+            self.y -= 1
+        self.struct = np.rot90(self.struct)
+
+    
+    def update(self):
+        if self.current:
+            self.move_down()
+
+
+
+class SquareBlock(Block):
+    struct = (
+        (1, 1),
+        (1, 1)
+    )
+
+class TBlock(Block):
+    struct = (
+        (1, 1, 0),
+        (0, 1, 0)
+    )
+
+class LineBlock(Block):
+    struct = (
+        (1,),
+        (1,),
+        (1,),
+        (1)
+    )
+
+class LBlock(Block):
+    struct = (
+        (1, 1),
+        (1, 0),
+        (1, 0),
+    )
+class ZBlock(Block):
+    struct = (
+        (0, 1),
+        (1, 1),
+        (1, 0),
+    )
+
+
+class BlocksGroup(pygame.sprite.OrderedUpdates):
+    
+    @staticmethod
+    def get_random_block():
+        return random.choice(
+            (SquareBlock, TBlock, LineBlock, LBlock, ZBlock)) ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self._reset_grid()
+        self._ignore_next_stop = False
+        self.score = 0
+        self.next_block = None
+        self.stop_moving_current_block()
+        self._create_new_block()
+
+    def _check_line_completion(self):
+        for i, row in enumerate(self.grid[::-1]):
+            if all(row):
+                self.score += 5
+                affected_blocks = list(
+                    OrderedDict.fromkeys(self.grid[-1 - i]))
+
+                for block, y_offset in affected_blocks:
+                    block.struct = np.delete(block.struct, y_offset, 0)
+                    if block.struct.any():
+                        block.struct, x_offset = \
+                            remove_empty_columns(block.struct)
+                        block.x += x_offset
+                        block.redraw()
+                    else:
+                        self.remove(block)
+
+
+                for block in self:
+                    if block.current:
+                        continue
+                    while True:
+                        try:
+                            block.move_down(self)
+                        except BottomReached:
+                            break
+
+                self.update_grid():
+                self._check_line_completion()
+                break
+
+
+    def _reset_grid(self):
+        self.grid = [[0 for _ in range(10)] for _ in range(20)]
+
+    def _create_new_block(self):
+        new_block = self.next_block or BlocksGroup.get_random_block()
+        if Block.collide(new_block, self):
+            raise TopReached
+        self.add(new_block)
+        self.next_block = BlocksGroup.get_random_block()
+        self.update_grid()
+        self._check_line_completion()
+
+    def update_grid(self):
+        self._reset_grid()
+        for block in self:
+            for y_offset, row in enumerate(block.struct):
+                for x_offset, digit in enumerate(row):
+                    if digit == 0:
+                        continue
+                    rowid = block.y + y_offset
+                    colid = block.x + x_offset
+                    self.grid[rowid][colid] = (block, y_offset)
+
+    @property
+    def current_block(self):
+        return self.sprites()[-1]
+
+    def update_current_block(self):
+        try:
+            self.current_block, move_down(self)
+        except BottomReached:
+            self.stop_moving_current_block()
+            self._create_new_block()
+        else:
+            self.update_grid()
+        
+
